58b6bb9de7cb1f7a428f60339040c95b71fbd074
isaac <isaactonyloi@gmail.com>
2022-11-29 08:58:15 +0300

    loops

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+//const gifts = ["teddy bear", "drone", "doll"];
+
+let result = [];
+function writeCards(gifts, event) {
+    //let result = [];
+    for (let i = 0; i < gifts.length; i++) {
+        let message = `Thank you, ${gifts[i]}, for the wonderful ${event} gift!`;
+        result.push(message);
+
+  }
+  
+  return result;
+}
+
+writeCards(gifts, "birthday");
+
+
+function countDown(num) {
+    let countdown = 0;
+    while (countdown <= num) {
+        console.log(num--);
+
+    }
+
+}
